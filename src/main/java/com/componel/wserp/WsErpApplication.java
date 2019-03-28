ae101b3106c949fe8f5a284533331e7473c735a0
Elias Araujo Sobrinho Junior <elias.junior@grupois.mao>
2019-03-28 11:15:08 -0400

    create project

---
+++ src/main/java/com/componel/wserp/WsErpApplication.java
@@ -0,0 +1,13 @@
+package com.componel.wserp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsErpApplication.class, args);
+	}
+
+}
